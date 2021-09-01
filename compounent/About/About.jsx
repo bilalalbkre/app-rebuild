c03248848610221531403a6b17926f422dcc39c8
bilalalbkre <blaloalbkre@gmail.com>
2021-09-02 02:34:17 +0300

    fix-6

--- compounent/About/About.jsx
+++ compounent/About/About.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import * as S from './style'
-import { attributes, react as HomeContent } from '../../content/home.md';
+// import { attributes, react as HomeContent } from '../../content/home.md';
 
 const AboutCont = () => {
-    let { title, cats } = attributes;
+    // let { title, cats } = attributes;
     //console.log('test 1 : ' ,title)
     return (
         <>
-            
-                <h1> {title} </h1>
+            {/* <S.AboutContent> */}
+                {/* <h1> {title} </h1> */}
                     <div style={{height:'500px'}}>
-                     {cats.map((cat,indx) => <div key={indx}><h1>{cat.name}</h1><p>{cat.description}</p></div> )}
+                     {/* {cats.map((cat,indx) => <div key={indx}><h1>{cat.name}</h1><p>{cat.description}</p></div> )} */}
                     </div>
-            
+            {/* </S.AboutContent> */}
         </>
     )
 }
